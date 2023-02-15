c432abf1113115ee261254bb2319fef6f74e0634
Josh <hashimotojosh@gmail.com>
2023-02-14 16:20:24 -0800

    added auth context

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,33 @@
+import { createContext, useReducer, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload }; //user is what we get back from the server when they are logged in
+    case "LOGOUT":
+      return { user: null };
+    default:
+      return state; //return original state if no action is taken
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, { user: null }); //user is null because we don't have a user logged in at the beginning usually
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    } //this allows us to keep being logged in even after refreshing the page
+  }, []);
+
+  console.log("AuthContext state: ", state); //this keeps track of the state in console (useful to know)
+  //
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { CardsContextProvider } from "./context/UserToolsContext";
+import { AuthContextProvider } from "./context/AuthContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <CardsContextProvider>
-      <App />
-    </CardsContextProvider>
+    <AuthContextProvider>
+      <CardsContextProvider>
+        <App />
+      </CardsContextProvider>
+    </AuthContextProvider>
   </BrowserRouter>
 );
 
